f8d33bfc0107f92315ee61ca47aa12753a527f90
Nobuchika Tanaka <sleepingdog.7799@gmail.com>
2022-10-04 03:52:07 +0900

    Change the number arg type to []int

--- sum.go
+++ sum.go
@@ -1,7 +1,7 @@
 package main
 
-// Sum calculates the total from an array of numbers.
-func Sum(numbers [5]int) int {
+// Sum calculates the total from a slice of numbers.
+func Sum(numbers []int) int {
 	sum := 0
 	for _, numbers := range numbers {
 		sum += numbers
