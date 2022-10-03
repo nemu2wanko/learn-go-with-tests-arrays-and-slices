70aefca41c10506ce81f781575d89c200ccddcad
Nobuchika Tanaka <sleepingdog.7799@gmail.com>
2022-10-04 03:40:52 +0900

    Refactor Sum()

--- sum.go
+++ sum.go
@@ -3,8 +3,8 @@ package main
 // Sum calculates the total from an array of numbers.
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, numbers := range numbers {
+		sum += numbers
 	}
 	return sum
 }
