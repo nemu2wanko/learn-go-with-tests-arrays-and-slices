c6d477f00e9b5541be41b6e2b4bf37ef71b0d274
Nobuchika Tanaka <sleepingdog.7799@gmail.com>
2022-10-05 03:50:52 +0900

    Add named return value to all functions

--- sum.go
+++ sum.go
@@ -1,8 +1,7 @@
 package main
 
 // Sum calculates the total from a slice of numbers.
-func Sum(numbers []int) int {
-	sum := 0
+func Sum(numbers []int) (sum int) {
 	for _, numbers := range numbers {
 		sum += numbers
 	}
@@ -10,8 +9,7 @@ func Sum(numbers []int) int {
 }
 
 // SumAll calculates the respective sums of every slice passed in.
-func SumAll(numbersToSum ...[]int) []int {
-	var sums []int
+func SumAll(numbersToSum ...[]int) (sums []int) {
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
 	}
@@ -19,8 +17,7 @@ func SumAll(numbersToSum ...[]int) []int {
 }
 
 // SumAllTails calculates the sums of all but the first number given a collection of slices.
-func SumAllTails(numbersToSum ...[]int) []int {
-	var sums []int
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
 	for _, numbers := range numbersToSum {
 		if len(numbers) == 0 {
 			sums = append(sums, 0)
